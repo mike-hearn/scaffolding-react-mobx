04563db4f778f6f43b413e53c26967ff94d9c040
Michael Hearn <git@mikehearn.net>
2017-04-12 20:55:08 -0400

    Update dependencies and webpack 2 config

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,139 @@
-const webpack = require('webpack');
-const path = require('path');
-const cssnext = require('postcss-cssnext');
-const precss = require('precss');
+const webpack = require("webpack");
+const path = require("path");
+const cssnext = require("postcss-cssnext");
+const precss = require("precss");
 
 module.exports = {
   entry: [
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    './src/index.jsx'
+    "webpack-dev-server/client?http://localhost:8080",
+    "webpack/hot/only-dev-server",
+    "./src/index.jsx"
   ],
   output: {
-    path: __dirname + '/dist',
-    publicPath: '/',
-    filename: 'bundle.js'
+    path: __dirname + "/dist",
+    publicPath: "/",
+    filename: "bundle.js"
   },
   resolve: {
-    extensions: ['', '.js', '.jsx', '.css', '.scss'],
+    extensions: [".js", ".jsx", ".css", ".scss"]
   },
   devServer: {
-    contentBase: './dist',
+    contentBase: "./dist",
     hot: true
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.jsx?$/,
-        loaders: ['react-hot', 'babel'],
-        include: path.join(__dirname, 'src')
+        use: [
+          {
+            loader: "react-hot-loader"
+          },
+          {
+            loader: "babel-loader"
+          }
+        ],
+        include: path.join(__dirname, "src")
       },
       {
         test: /\.css$/,
-        exclude: ['node_modules'],
-        loader: 'style-loader!css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
+        exclude: ["node_modules"],
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]__[local]___[hash:base64:5]"
+            }
+          },
+          {
+            loader: "postcss-loader"
+          }
+        ]
       },
       // Exclude global SASS files (in src/shared/styles) from CSS modules processing
       {
         test: /\.scss$/,
-        exclude: [__dirname + '/src/shared/styles'],
-        loader: 'style!css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss?parser=postcss-scss!sass?sourceMap'
+        exclude: [__dirname + "/src/shared/styles"],
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]__[local]___[hash:base64:5]"
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              parser: "postcss-scss"
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
       },
       // Globally scoped SASS files (including bootstrap) should not be processed through CSS Modules
       {
         test: /\.scss$/,
-        include: [__dirname + '/src/shared/styles'],
-        loader: 'style!css!postcss?parser=postcss-scss!sass?sourceMap'
+        include: [__dirname + "/src/shared/styles"],
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              parser: "postcss-scss"
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
       },
       {
         test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 10000,
+              mimetype: "application/font-woff"
+            }
+          }
+        ]
       },
       {
         test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: 'file-loader'
+        loader: "file-loader"
       }
-    ],
-  },
-  postcss: function postcss() {
-    return [cssnext, precss];
+    ]
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.Tether': 'tether',
-    }),
+      $: "jquery",
+      jQuery: "jquery",
+      "window.Tether": "tether"
+    })
   ]
 };
-
